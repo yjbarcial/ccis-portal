23f007f4118aa1ff6f7df762d8100788d6462058
yjbarcial <yjbarcial@gmail.com>
2025-04-16 21:09:59 +0800

    feat: added password view func

--- src/components/auth/RegisterForm.vue
+++ src/components/auth/RegisterForm.vue
@@ -1,3 +1,10 @@
+<script setup>
+import { ref } from 'vue'
+
+const isPasswordvisible = ref(false)
+const isConfirmPasswordvisible = ref(false)
+</script>
+
 <template>
   <v-form @submit.prevent class="register-form">
     <!-- Form Header -->
@@ -39,9 +46,12 @@
         <v-text-field
           label="Password"
           type="password"
-          variant="solo-filled"
           prepend-inner-icon="mdi-lock"
           class="mb-3"
+          variant="solo-filled"
+          :append-inner-icon="isPasswordvisible ? 'mdi-eye' : 'mdi-eye-off'"
+          :type="isPasswordvisible ? 'text' : 'password'"
+          @click:append-inner="isPasswordvisible = !isPasswordvisible"
         ></v-text-field>
       </v-col>
       <v-col cols="12" sm="6">
@@ -52,6 +62,9 @@
           variant="solo-filled"
           prepend-inner-icon="mdi-lock-check"
           class="mb-3"
+          :append-inner-icon="isConfirmPasswordvisible ? 'mdi-eye' : 'mdi-eye-off'"
+          :type="isConfirmPasswordvisible ? 'text' : 'password'"
+          @click:append-inner="isConfirmPasswordvisible = !isConfirmPasswordvisible"
         ></v-text-field>
       </v-col>
     </v-row>
@@ -72,7 +85,7 @@
 
 <style scoped>
 .register-form {
-  max-width: 520px;  /* Increased max-width */
+  max-width: 520px; /* Increased max-width */
   width: 100%;
   padding: 30px;
   border-radius: 12px;
