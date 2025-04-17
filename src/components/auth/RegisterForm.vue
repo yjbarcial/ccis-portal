0c827a09775cf74669b92f9d2d9ded058c9b1e77
yjbarcial <yjbarcial@gmail.com>
2025-04-17 17:06:19 +0800

    feat: added supabase.js; added registration functionality

--- src/components/auth/RegisterForm.vue
+++ src/components/auth/RegisterForm.vue
@@ -1,12 +1,96 @@
 <script setup>
+import {
+  requiredValidator,
+  emailValidator,
+  passwordValidator,
+  confirmedValidator,
+} from '@/utils/validators'
 import { ref } from 'vue'
+import { supabase, formActionDefault } from '@/utils/supabase.js'
+
+const formDataDefault = {
+  firstname: '',
+  lastname: '',
+  email: '',
+  password: '',
+  password_confirmation: '',
+}
+
+const formData = ref({
+  ...formDataDefault,
+})
+
+const formAction = ref({
+  ...formActionDefault,
+})
 
 const isPasswordvisible = ref(false)
 const isConfirmPasswordvisible = ref(false)
+const refVForm = ref()
+
+const onSubmit = async () => {
+  formAction.value = { ...formActionDefault }
+  formAction.value.formProcess = true
+  // alert(formData.value.employeeID)
+  const { data, error } = await supabase.auth.signUp({
+    email: formData.value.email,
+    password: formData.value.password,
+    options: {
+      data: {
+        firstname: formData.value.firstname,
+        lastname: formData.value.lastname,
+      },
+    },
+  })
+  if (error) {
+    console.log(error)
+    formAction.value.formErrorMessage = error.message
+    formAction.value.formStatus = error.status
+  } else if (data) {
+    console.log(data)
+    formAction.value.formSuccessMessage = 'You have registered an account!'
+    // Add here more actions if you want
+    refVForm.value?.reset()
+  }
+
+  formAction.value.formProcess = false
+}
+
+const onFormSubmit = () => {
+  refVForm.value?.validate().then(({ valid }) => {
+    if (valid) onSubmit()
+  })
+}
 </script>
 
 <template>
-  <v-form @submit.prevent class="register-form">
+  <!-- Alert Message: Success -->
+  <v-alert
+    v-if="formAction.formSuccessMessage"
+    :text="formAction.formSuccessMessage"
+    class="mb-4"
+    title="Success!"
+    type="success"
+    variant="tonal"
+    density="compact"
+    border="start"
+    closable
+  ></v-alert>
+
+  <!-- Alert Message: Error -->
+  <v-alert
+    v-if="formAction.formErrorMessage"
+    :text="formAction.formErrorMessage"
+    class="mb-4"
+    title="Oops!"
+    type="error"
+    variant="tonal"
+    density="compact"
+    border="start"
+    closable
+  ></v-alert>
+
+  <v-form @submit.prevent="onFormSubmit" ref="refVForm" class="register-form">
     <!-- Form Header -->
     <div class="text-center mb-5">
       <v-icon size="36" color="deep-orange">mdi-account-plus</v-icon>
@@ -17,60 +101,78 @@ const isConfirmPasswordvisible = ref(false)
     <v-row>
       <v-col cols="12" sm="6">
         <v-text-field
+          v-model="formData.firstname"
           label="First Name"
           variant="solo-filled"
           prepend-inner-icon="mdi-account"
           class="mb-3"
+          :rules="[requiredValidator]"
         ></v-text-field>
       </v-col>
       <v-col cols="12" sm="6">
         <v-text-field
+          v-model="formData.lastname"
           label="Last Name"
           variant="solo-filled"
           prepend-inner-icon="mdi-account"
           class="mb-3"
+          :rules="[requiredValidator]"
         ></v-text-field>
       </v-col>
     </v-row>
 
     <!-- Email -->
     <v-text-field
+      v-model="formData.email"
       label="Email Address"
       variant="solo-filled"
       prepend-inner-icon="mdi-email"
       class="mb-3"
+      :rules="[requiredValidator, emailValidator]"
     ></v-text-field>
     <v-row>
       <v-col cols="12" sm="6">
         <!-- Password -->
         <v-text-field
+          v-model="formData.password"
           label="Password"
-          type="password"
+          variant="solo-filled"
           prepend-inner-icon="mdi-lock"
           class="mb-3"
-          variant="solo-filled"
           :append-inner-icon="isPasswordvisible ? 'mdi-eye' : 'mdi-eye-off'"
           :type="isPasswordvisible ? 'text' : 'password'"
           @click:append-inner="isPasswordvisible = !isPasswordvisible"
+          :rules="[requiredValidator, passwordValidator]"
         ></v-text-field>
       </v-col>
       <v-col cols="12" sm="6">
         <!-- Confirm Password -->
         <v-text-field
+          v-model="formData.password_confirmation"
           label="Confirm Password"
-          type="password"
           variant="solo-filled"
           prepend-inner-icon="mdi-lock-check"
           class="mb-3"
           :append-inner-icon="isConfirmPasswordvisible ? 'mdi-eye' : 'mdi-eye-off'"
           :type="isConfirmPasswordvisible ? 'text' : 'password'"
           @click:append-inner="isConfirmPasswordvisible = !isConfirmPasswordvisible"
-        ></v-text-field>
+          :rules="[requiredValidator, confirmedValidator(password_confirmation)]"
+        />
       </v-col>
     </v-row>
 
     <!-- Register Button -->
-    <v-btn type="submit" block color="deep-orange" size="large" elevation="3"> Register </v-btn>
+    <v-btn
+      type="submit"
+      block
+      color="deep-orange"
+      size="large"
+      elevation="3"
+      :disabled="formAction.formProcess"
+      :loading="formAction.formProcess"
+    >
+      Register
+    </v-btn>
 
     <!-- Divider -->
     <v-divider class="my-5"></v-divider>
