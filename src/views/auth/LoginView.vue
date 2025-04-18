5f9b4f1c077f9b474115e73e32e828c3b5fb19e1
lhclavel <lovellclavel14@gmail.com>
2025-04-18 22:21:32 +0800

    fix: images path

--- src/views/auth/LoginView.vue
+++ src/views/auth/LoginView.vue
@@ -48,7 +48,7 @@ const { mobile } = useDisplay()
       <v-row class="d-flex justify-center">
         <v-col class="text-center">
           <v-avatar size="24" class="mr-2">
-            <v-img src="/public/images/ccisLogo.png" alt="CCIS Logo" />
+            <v-img src="/images/ccisLogo.png" alt="CCIS Logo" />
           </v-avatar>
           <span class="text-white">&copy; 2025 CCIS. All Rights Reserved.</span>
         </v-col>
