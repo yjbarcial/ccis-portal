8c5a25ebf51e146f298dcb277ace0788939e60ef
lhclavel <lovellclavel14@gmail.com>
2025-04-28 22:13:19 +0800

    style: adjust margins

--- src/views/auth/LoginView.vue
+++ src/views/auth/LoginView.vue
@@ -9,14 +9,13 @@ const { mobile } = useDisplay()
 <template>
   <v-container
     fluid
-    class="d-flex flex-column justify-between pa-0"
+    class="d-flex flex-column justify-between pa-4"
     style="min-height: 100vh; position: relative"
   >
     <!-- Background Image -->
     <v-img
       src="/images/ccisHiraya.jpg"
       cover
-      
       class="position-absolute"
       style="
         inset: 0;
@@ -30,7 +29,6 @@ const { mobile } = useDisplay()
 
     <!-- Content -->
     <v-row class="flex-grow-1" align="center" justify="center">
-      <v-container></v-container>
       <v-col cols="12" md="5" class="text-center">
         <!-- Logo -->
         <v-img src="/images/ccisLogo.png" :width="mobile ? '65%' : '120px'" class="mx-auto mb-4" />
