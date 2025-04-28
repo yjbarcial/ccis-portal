2be1c8e9f6d6875defa615c6e1832dc3eb015d64
lhclavel <lovellclavel14@gmail.com>
2025-04-28 16:35:33 +0800

    chore: removing multiple footers in one view

--- src/views/auth/RegisterView.vue
+++ src/views/auth/RegisterView.vue
@@ -58,15 +58,10 @@ const { mobile } = useDisplay()
     <v-footer class="py-3" style="background-color: transparent; position: relative; width: 100%">
       <v-row class="d-flex justify-center">
         <v-col class="text-center">
-          <span class="text-white">© 2025 CCIS Portal. All Rights Reserved.</span>
           <v-avatar size="24" class="mr-2">
             <v-img src="/images/ccisLogo.png" alt="CCIS Logo" />
           </v-avatar>
           <span class="text-white">© 2025 CCIS. All Rights Reserved.</span>
-          <v-avatar size="24" class="mr-2">
-            <v-img src="/images/ccisLogo.png" alt="CCIS Logo" />
-          </v-avatar>
-          <span class="text-white">&copy; 2025 CCIS. All Rights Reserved.</span>
         </v-col>
       </v-row>
     </v-footer>
