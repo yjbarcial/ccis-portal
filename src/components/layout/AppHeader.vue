0cca56695938e4f03ef073f9714d4e1c12647ae8
lhclavel <lovellclavel14@gmail.com>
2025-04-29 00:21:00 +0800

    feat(app-header): added icon logo to the left of title

--- src/components/layout/AppHeader.vue
+++ src/components/layout/AppHeader.vue
@@ -27,12 +27,24 @@ const handleLogout = async () => {
 <template>
   <v-app-bar color="orange-darken-4" dark flat app>
     <v-container fluid class="d-flex align-center justify-space-between">
-      <v-toolbar-title class="text-h6">{{ title }}</v-toolbar-title>
+      <v-toolbar-title>
+        <div class="d-flex align-center" style="cursor: pointer" @click="goTo('dashboard')">
+          <v-img
+            src="/images/ccisLogo.png"
+            alt="CCIS Logo"
+            max-width="30"
+            height="30"
+            class="mr-1"
+            cover
+          />
+          <span>{{ title }}</span>
+        </div>
+      </v-toolbar-title>
+
       <div class="d-flex align-center gap-4">
         <v-btn variant="text" @click="goTo('dashboard')" class="text-white">Dashboard</v-btn>
         <v-btn variant="text" @click="goTo('syllabi')" class="text-white">Syllabi</v-btn>
         <v-btn variant="text" @click="goTo('theses')" class="text-white">Theses</v-btn>
-        
 
         <!-- User Menu -->
         <v-menu location="bottom end">
