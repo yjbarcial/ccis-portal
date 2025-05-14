be1d26ff69493e808a2caefa82576c1ede3bf5ca
lhclavel <lovellclavel14@gmail.com>
2025-05-14 10:25:10 +0800

    feat: restore dark mode toggle button

--- src/components/layout/AppHeader.vue
+++ src/components/layout/AppHeader.vue
@@ -164,6 +164,28 @@ const props = defineProps({
       <v-btn icon class="d-md-none" @click="drawer = !drawer">
         <v-icon>mdi-menu</v-icon>
       </v-btn>
+
+      <!-- From Uiverse.io by JustCode14 -->
+      <label class="switch">
+        <input
+          type="checkbox"
+          :checked="!isDark"
+          @change="toggleDarkMode"
+          style="z-index: 2; position: relative"
+        />
+        <span class="slider">
+          <div class="star star_1"></div>
+          <div class="star star_2"></div>
+          <div class="star star_3"></div>
+          <svg viewBox="0 0 16 16" class="cloud_1 cloud">
+            <path
+              transform="matrix(.77976 0 0 .78395-299.99-418.63)"
+              fill="#fff"
+              d="m391.84 540.91c-.421-.329-.949-.524-1.523-.524-1.351 0-2.451 1.084-2.485 2.435-1.395.526-2.388 1.88-2.388 3.466 0 1.874 1.385 3.423 3.182 3.667v.034h12.73v-.006c1.775-.104 3.182-1.584 3.182-3.395 0-1.747-1.309-3.186-2.994-3.379.007-.106.011-.214.011-.322 0-2.707-2.271-4.901-5.072-4.901-2.073 0-3.856 1.202-4.643 2.925"
+            ></path>
+          </svg>
+        </span>
+      </label>
     </v-container>
   </v-app-bar>
 
